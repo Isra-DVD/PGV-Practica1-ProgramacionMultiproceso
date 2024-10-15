f3d25eb2e0d0d0a55ae7028c5b59bec786ea7790
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-10-15 20:19:42 +0100

    feat(FileHelper): Create class FileHelper with a function called  getAllLines

---
+++ src/net/salesianos/utils/FileHelper.java
@@ -0,0 +1,27 @@
+package net.salesianos.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class FileHelper {
+    public static ArrayList<String> getAllLines(String fileRoute) {
+        ArrayList<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileRoute, StandardCharsets.UTF_8));) {
+            String currentLine = reader.readLine();
+
+            while (currentLine != null) {
+                lines.add(currentLine);
+                currentLine = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("No se han podido obtener las lineas del fichero " + fileRoute);
+        }
+
+        return lines;
+    }
+}
